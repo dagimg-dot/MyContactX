988f69d3a348b3d9fa5cd35d3c16664687fc0e4f
Dagim G. Astatkie <workflow.jd@gmail.com>
2023-03-29 19:40:04 +0300

    add search bar to the UI as a component

--- src/App.java
+++ src/App.java
@@ -5,6 +5,8 @@ import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
 
+import components.search.SearchTextField;
+
 public class App extends Application {
     public static void main(String[] args) throws Exception {
         launch(args);
@@ -22,23 +24,38 @@ public class App extends Application {
         mainWindow.setArcHeight(15);
         mainWindow.setArcWidth(15);
 
+        // element 1 - search bar (inside main window) -> text field
+        SearchTextField searchBar = new SearchTextField("Search", 361, 58, 281, 33);
+        searchBar.getStylesheets().add("components/search/SearchTextField.css");
+
+        /*
+         * search icon is not added yet, because i don't know how.
+         * some one should add it.
+         */
+
+        // element 2 - hamburger menu (inside main window) -> button
+        /*
+         * hamburger menu is not added yet.
+         * some one should add it.
+         */
+        
         // second rectangle - bottom bar
         Rectangle bottomBar = new Rectangle(24, 936, 897, 71);
         bottomBar.setFill(Color.valueOf("D9D9D9"));
         bottomBar.setArcHeight(15);
         bottomBar.setArcWidth(15);
-
+        
         // third rectangle - left bar
         Rectangle leftBar = new Rectangle(937, 17, 478, 990);
         leftBar.setFill(Color.valueOf("D9D9D9"));
         leftBar.setArcHeight(15);
         leftBar.setArcWidth(15);
         
-
         // add rectangles to root
-        root.getChildren().addAll(mainWindow,bottomBar,leftBar);
-
-        primaryStage.setScene(new Scene(root, 1440, 1024));
+        root.getChildren().addAll(mainWindow,bottomBar,leftBar,searchBar);
+        
+        Scene scene = new Scene(root, 1440, 1024);
+        primaryStage.setScene(scene);
         primaryStage.show();
     }
 }
