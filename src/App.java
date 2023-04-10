41c27183318023392b02c2e7895960d1cdd0c74a
Dagim G. Astatkie <workflow.jd@gmail.com>
2023-04-11 00:54:27 +0300

    code refactor to make it clean and take design patterns into consideration

--- src/App.java
+++ src/App.java
@@ -1,11 +1,15 @@
 import javafx.application.Application;
 import javafx.scene.Scene;
-import javafx.scene.layout.Pane;
-import javafx.scene.paint.Color;
-import javafx.scene.shape.Rectangle;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.ColumnConstraints;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.RowConstraints;
 import javafx.stage.Stage;
 
 import components.search.SearchTextField;
+import components.container.ContainerFactory;
+import components.container.Container;
+
 
 public class App extends Application {
     public static void main(String[] args) throws Exception {
@@ -14,48 +18,77 @@ public class App extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        Pane root = new Pane();
         primaryStage.setTitle("Contact Manager");
-        primaryStage.setResizable(false);    // disabled resizing: because it's not supported yet and i am copying the coordinates and the size of the window from the design (figma)
 
-        // first rectangle - main window
-        Rectangle mainWindow = new Rectangle(24,17, 897, 905);
-        mainWindow.setFill(Color.valueOf("D9D9D9"));
-        mainWindow.setArcHeight(15);
-        mainWindow.setArcWidth(15);
+        AnchorPane root = new AnchorPane();
 
-        // element 1 - search bar (inside main window) -> text field
-        SearchTextField searchBar = new SearchTextField("Search", 361, 58, 281, 33);
-        searchBar.getStylesheets().add("components/search/SearchTextField.css");
+        Scene scene = new Scene(root, 1200,768);
+        
+        // set the min width and height of the window
 
-        /*
-         * search icon is not added yet, because i don't know how.
-         * some one should add it.
-         */
+        primaryStage.setMinWidth(1200);
+        primaryStage.setMinHeight(768);
+        
+        GridPane grid = new GridPane();
 
-        // element 2 - hamburger menu (inside main window) -> button
-        /*
-         * hamburger menu is not added yet.
-         * some one should add it.
-         */
-        
-        // second rectangle - bottom bar
-        Rectangle bottomBar = new Rectangle(24, 936, 897, 71);
-        bottomBar.setFill(Color.valueOf("D9D9D9"));
-        bottomBar.setArcHeight(15);
-        bottomBar.setArcWidth(15);
-        
-        // third rectangle - left bar
-        Rectangle leftBar = new Rectangle(937, 17, 478, 990);
-        leftBar.setFill(Color.valueOf("D9D9D9"));
-        leftBar.setArcHeight(15);
-        leftBar.setArcWidth(15);
-        
-        // add rectangles to root
-        root.getChildren().addAll(mainWindow,bottomBar,leftBar,searchBar);
-        
-        Scene scene = new Scene(root, 1440, 1024);
+        // create container to be added to the grid
+
+        ContainerFactory containerFactory = new ContainerFactory(scene);
+
+        Container mainContainer = containerFactory.getType("MAIN");
+        Container bottomContainer = containerFactory.getType("BOTTOM");
+        Container leftContainer = containerFactory.getType("LEFT");
+
+        // add the containers to the grid
+
+        grid.add(mainContainer, 0, 0);
+        grid.add(bottomContainer, 0, 1);
+        grid.add(leftContainer, 1, 0, 1, 2);
+
+        // Set the column and row constraints for the GridPane
+
+        setConstraints(grid, scene);
+
+        // set padding for the anchor pane
+        AnchorPane.setTopAnchor(grid, 15.0);
+        AnchorPane.setBottomAnchor(grid, 15.0);
+        AnchorPane.setLeftAnchor(grid, 15.0);
+        AnchorPane.setRightAnchor(grid, 15.0);
+
+        // set the gap between the rows and columns
+        grid.setHgap(15);
+        grid.setVgap(15);
+
+        // Add the GridPane to the root
+        root.getChildren().add(grid);
+
+        // set the scene and show the stage
+
         primaryStage.setScene(scene);
         primaryStage.show();
+
+        // add the search text field to the main container
+
+        SearchTextField searchTextField = new SearchTextField("Search", 250, 28);
+        searchTextField.setPosition(mainContainer.getWidth(), mainContainer.getHeight());
+
+        mainContainer.getChildren().add(searchTextField);
+
+    }
+
+    public void setConstraints(GridPane grid, Scene scene) {
+        ColumnConstraints col1 = new ColumnConstraints();
+        col1.setPercentWidth(scene.getWidth() * 0.65);
+        ColumnConstraints col2 = new ColumnConstraints();
+        col2.setPercentWidth(scene.getWidth() * 0.35);
+
+        grid.getColumnConstraints().addAll(col1, col2);
+
+        RowConstraints row1 = new RowConstraints();
+        row1.setPercentHeight(scene.getHeight() * 0.9);
+        RowConstraints row2 = new RowConstraints();
+        row2.setPercentHeight(scene.getHeight() * 0.1);
+
+        grid.getRowConstraints().addAll(row1, row2);
     }
 }
