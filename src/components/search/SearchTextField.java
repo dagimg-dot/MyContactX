a24ffe994133fed634754ce54f596cd73c88ffe8
Dagim G. Astatkie <workflow.jd@gmail.com>
2023-04-11 00:53:05 +0300

    update searchtextfield component with the new layoute

--- src/components/search/SearchTextField.java
+++ src/components/search/SearchTextField.java
@@ -3,13 +3,19 @@ package components.search;
 import javafx.scene.control.TextField;
 
 public class SearchTextField extends TextField {
+    private Double _width;
     
-    public SearchTextField(String promptText, double width, double height, double layoutX, double layoutY) {
+    public SearchTextField(String promptText, double width, double height) {
         super();
+        this._width = width;
         setPrefSize(width, height);
-        setLayoutX(layoutX);
-        setLayoutY(layoutY);
         setPromptText(promptText);
+        this.getStylesheets().add("components/search/SearchTextField.css");
         this.getStyleClass().add("search-text-field");
+    }
+
+    public void setPosition(double mWidth, double mHeight) {
+        this.setLayoutX((mWidth - _width)/2);
+        this.setLayoutY(mHeight*0.03);
     }
 }
