59ac4704c80b01d721e70e2f9210512517b26d49
Dagim G. Astatkie <workflow.jd@gmail.com>
2023-04-13 00:13:22 +0300

    add search icon before the prompt text in the search text field

--- src/components/search/SearchTextField.java
+++ src/components/search/SearchTextField.java
@@ -1,21 +1,29 @@
 package components.search;
 
+import de.jensd.fx.glyphs.fontawesome.FontAwesomeIcon;
+import de.jensd.fx.glyphs.fontawesome.FontAwesomeIcons;
+import javafx.geometry.Pos;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
 
 public class SearchTextField extends TextField {
-    private Double _width;
+    private Pane searchIconContainer;
     
     public SearchTextField(String promptText, double width, double height) {
         super();
-        this._width = width;
         setPrefSize(width, height);
+        FontAwesomeIcon searchIcon = new FontAwesomeIcon();
+        searchIcon.setIcon(FontAwesomeIcons.SEARCH);
+        searchIcon.setFill(Color.valueOf("#625E5E"));
+        searchIconContainer = new HBox(searchIcon);
+        searchIconContainer.getStyleClass().add("pane");
+        searchIconContainer.getStyleClass().add("search-icon-container");
+        getChildren().add(0,searchIconContainer);
         setPromptText(promptText);
+        setAlignment(Pos.CENTER);
         this.getStylesheets().add("components/search/SearchTextField.css");
         this.getStyleClass().add("search-text-field");
-    }
-
-    public void setPosition(double mWidth, double mHeight) {
-        this.setLayoutX((mWidth - _width)/2);
-        this.setLayoutY(mHeight*0.03);
     }
 }
