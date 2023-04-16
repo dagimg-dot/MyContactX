4ca08a3a0f3550b18949c4a20c5b4909e7a164b1
Dagim G. Astatkie <workflow.jd@gmail.com>
2023-04-16 18:35:23 +0300

    add phone button controller

---
+++ src/components/button/phoneButton/PhoneButtonController.java
@@ -0,0 +1,9 @@
+package components.button.phoneButton;
+
+import controller.Controller;
+
+public class PhoneButtonController extends Controller {
+    public void onClick() {
+       System.out.println("Phone button clicked");
+    }
+}
