57c42fd13b227f953c7fe79801572d2451d5a59a
Dagim G. Astatkie <workflow.jd@gmail.com>
2023-04-16 18:35:06 +0300

    add name button controller

---
+++ src/components/button/nameButton/NameButtonController.java
@@ -0,0 +1,9 @@
+package components.button.nameButton;
+
+import controller.Controller;
+
+public class NameButtonController extends Controller {
+    public void onClick() {
+       System.out.println("Name button clicked");
+    }
+}
