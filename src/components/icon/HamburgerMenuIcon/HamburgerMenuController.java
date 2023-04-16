a91131771bd609b41ef079dc2d1c6adf8768f966
Dagim G. Astatkie <workflow.jd@gmail.com>
2023-04-16 17:05:37 +0300

    add controller for the hamburder menu button

---
+++ src/components/icon/HamburgerMenuIcon/HamburgerMenuController.java
@@ -0,0 +1,13 @@
+package components.icon.HamburgerMenuIcon;
+
+import controller.Controller;
+
+public class HamburgerMenuController extends Controller {
+    public HamburgerMenuController() {
+        super();
+    }
+
+    public void openMenu() {
+        System.out.println("Open Menu");
+    }
+}
