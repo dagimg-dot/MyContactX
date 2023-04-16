d2e36104ed456ad24d5b1e07f6e6e86eb2656b0f
Dagim G. Astatkie <workflow.jd@gmail.com>
2023-04-16 17:06:23 +0300

    add controller and css file the searchIcon button

---
+++ src/components/icon/searchIcon/SearchIconController.java
@@ -0,0 +1,13 @@
+package components.icon.searchIcon;
+
+import controller.Controller;
+
+public class SearchIconController extends Controller {
+    public SearchIconController() {
+        super();
+    }
+
+    public void search() {
+        System.out.println("Searching");
+    }
+}
