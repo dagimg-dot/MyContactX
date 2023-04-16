b18a3b613ff46e4fb70efac4ef8916fb6ecad7d1
Dagim G. Astatkie <workflow.jd@gmail.com>
2023-04-16 18:34:45 +0300

    add group button controller

---
+++ src/components/button/groupButton/GroupButtonController.java
@@ -0,0 +1,9 @@
+package components.button.groupButton;
+
+import controller.Controller;
+
+public class GroupButtonController extends Controller {
+    public void onClick() {
+       System.out.println("Group button clicked");
+    }
+}
