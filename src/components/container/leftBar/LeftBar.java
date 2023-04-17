632bcf255d30aa86bb098de179b446748a6fe77e
Dagim G. Astatkie <workflow.jd@gmail.com>
2023-04-17 22:43:16 +0300

    add: add the details title card

--- src/components/container/leftBar/LeftBar.java
+++ src/components/container/leftBar/LeftBar.java
@@ -1,8 +1,14 @@
 package components.container.leftBar;
 
 import components.container._container.Container;
+import components.text.TextGenerator;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
 
 public class LeftBar extends Container{
     public LeftBar(Scene scene, double widthPercentage, double heightPercentage) {
@@ -15,5 +21,39 @@ public class LeftBar extends Container{
 
     public void render(Scene scene, GridPane grid) { 
         grid.add(this, 1, 0, 1, 2);
+
+
+        VBox leftBarContainer = new VBox();
+        leftBarContainer.prefWidthProperty().bind(this.widthProperty());
+        leftBarContainer.prefHeightProperty().bind(this.heightProperty());
+        leftBarContainer.setStyle("-fx-background-color: #D9D9D9; -fx-background-radius: 15;");
+
+        
+        HBox title = new HBox();
+        
+        // set the height and width of the title
+        title.setPrefHeight(this.getPrefHeight() / 15);
+        title.prefWidthProperty().bind(leftBarContainer.prefWidthProperty());
+
+        // create the title text
+        new TextGenerator();
+        Text details = TextGenerator.generateText("Details", 20, "#000000", "Times New Roman");
+
+        // set the alignment of the title
+        title.setAlignment(Pos.CENTER);
+        
+        // add the title text to the title HBox
+        title.getChildren().add(details);
+
+        // set margin for the title
+        VBox.setMargin(title, new Insets(10,10,10,10));
+
+        // set the style of the title
+        title.setStyle("-fx-background-color: #9B9595; -fx-background-radius: 15;");
+
+        // add the title to the left bar container
+        leftBarContainer.getChildren().addAll(title);
+        this.getChildren().addAll(leftBarContainer);
+
     };
 }
