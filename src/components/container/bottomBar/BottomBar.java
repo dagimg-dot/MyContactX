59e63871dc143d5e84533ee3a7752d6478d38b70
Dagim G. Astatkie <workflow.jd@gmail.com>
2023-04-17 15:51:32 +0300

    add: add the new,edit and delete buttons without the controllers enabled

--- src/components/container/bottomBar/BottomBar.java
+++ src/components/container/bottomBar/BottomBar.java
@@ -1,8 +1,13 @@
 package components.container.bottomBar;
 
+import components.button._button.ButtonFactory;
 import components.container._container.Container;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
 
 public class BottomBar extends Container {
     public BottomBar(Scene scene, double widthPercentage, double heightPercentage) {
@@ -15,5 +20,33 @@ public class BottomBar extends Container {
 
     public void render(Scene scene, GridPane grid) {
         grid.add(this, 0, 1);
+
+        HBox buttonContainer = new HBox();
+
+        buttonContainer.prefWidthProperty().bind(this.widthProperty());
+        buttonContainer.prefHeightProperty().bind(this.heightProperty());
+
+        ButtonFactory buttonFactory = new ButtonFactory();
+        // create the three buttons (new, edit, delete)
+        Button newButton = buttonFactory.createButton("New", 100.0, 28.0);
+        Button editButton = buttonFactory.createButton("Edit", 100.0, 28.0);
+        Button deleteButton = buttonFactory.createButton("Delete", 100.0, 28.0);
+
+        newButton.prefWidthProperty().bind(buttonContainer.widthProperty().divide(6));
+        editButton.prefWidthProperty().bind(buttonContainer.widthProperty().divide(6));
+        deleteButton.prefWidthProperty().bind(buttonContainer.widthProperty().divide(6));
+        
+        // set margin for the buttons created
+        HBox.setMargin(newButton, new Insets( 0, 0, 0, 15));
+        HBox.setMargin(editButton, new Insets( 0, 0, 0, 25));
+        HBox.setMargin(deleteButton, new Insets( 0, 15, 0, 25));
+
+        // add the buttons to the buttonContainer HBox
+        buttonContainer.getChildren().addAll(newButton,editButton,deleteButton);
+
+        // add the buttonContainer to the left bar
+        this.getChildren().addAll(buttonContainer);
+
+        buttonContainer.setAlignment(Pos.CENTER);
     };
 }
