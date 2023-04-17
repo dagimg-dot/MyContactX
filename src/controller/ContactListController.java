25d0e24a60209cfd281fe7814e89054752b219c2
Dagim G. Astatkie <workflow.jd@gmail.com>
2023-04-18 02:00:04 +0300

    add: add the contactListController for the Contact Data Model

---
+++ src/controller/ContactListController.java
@@ -0,0 +1,40 @@
+package controller;
+
+import java.util.ArrayList;
+
+import javafx.beans.Observable;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import models.Contact;
+
+public class ContactListController {
+    public ContactListController() {
+    }
+
+    public ObservableList<Contact> showContactList() {
+        ArrayList<Contact> contactList = new Contact().getContactList();
+        ObservableList<Contact> contacts = FXCollections.observableArrayList(contactList);
+        return contacts;
+    }
+
+    public void showContactGroup(String group) {
+        System.out.println("Showing Contact Group:" + group);
+    }
+    
+    public void addContact() {
+        System.out.println("Add Contact");
+    }
+
+    public void editContact() {
+        System.out.println("Edit Contact");
+    }
+
+    public void deleteContact() {
+        System.out.println("Delete Contact");
+    }
+
+    public void searchContact() {
+        System.out.println("Search Contact");
+    }
+
+}
