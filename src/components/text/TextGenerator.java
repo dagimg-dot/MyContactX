9c8d66d845520fed7ae81eb4b2291a1e85f9d293
Dagim G. Astatkie <workflow.jd@gmail.com>
2023-04-21 23:14:39 +0300

    refactor: addition of constructor for the text generator class

--- src/components/text/TextGenerator.java
+++ src/components/text/TextGenerator.java
@@ -3,9 +3,15 @@ package components.text;
 import javafx.scene.text.Text;
 
 public class TextGenerator {
-    public static Text generateText(String text, int fontSize, String fontColor, String fontFamily) {
+    public static Text generateText(String text, int fontSize, String fontColor, String fontFamily, String fontWeight) {
         Text _text = new Text(text);
-        _text.setStyle("-fx-font-size: " + fontSize + "px; -fx-text-fill: " + fontColor + "; -fx-font-family: " + fontFamily + ";");
+        _text.setStyle("-fx-font-size: " + fontSize + "px; -fx-color: " + fontColor + "; -fx-font-family: " + fontFamily + ";" + " -fx-font-weight: " + fontWeight + ";");
+        return _text;
+    }
+    
+    public static Text generateText(String text) {
+        Text _text = new Text(text);
+        _text.setStyle("-fx-font-size: " + 20 + "px; -fx-color: " + "#000000" + "; -fx-font-family: " + "Times New Roman" + ";");
         return _text;
     }
 }
