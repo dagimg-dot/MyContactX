cbe6f00095d8e95afcd6f6f6ebca76302640c75d
Dagim G. Astatkie <workflow.jd@gmail.com>
2023-04-21 23:13:21 +0300

    refactor: apply the change that comes with controller interface

--- src/components/icon/HamburgerMenuIcon/HamburgerMenuController.java
+++ src/components/icon/HamburgerMenuIcon/HamburgerMenuController.java
@@ -2,10 +2,15 @@ package components.icon.HamburgerMenuIcon;
 
 import controller.Controller;
 
-public class HamburgerMenuController extends Controller {
+public class HamburgerMenuController implements Controller {
     public HamburgerMenuController() {
         super();
     }
+    
+    public void onClick() {
+        openMenu();
+    }
+
 
     public void openMenu() {
         System.out.println("Open Menu");
