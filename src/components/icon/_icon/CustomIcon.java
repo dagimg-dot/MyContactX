01caba46b7295fafe30b88973a48a87f11da8ded
Dagim G. Astatkie <workflow.jd@gmail.com>
2023-04-21 23:12:41 +0300

    add: support for icons from file and without click event

--- src/components/icon/_icon/CustomIcon.java
+++ src/components/icon/_icon/CustomIcon.java
@@ -6,13 +6,17 @@ import controller.Controller;
 import de.jensd.fx.glyphs.fontawesome.FontAwesomeIcon;
 import de.jensd.fx.glyphs.fontawesome.FontAwesomeIcons;
 import javafx.scene.paint.Color;
+import javafx.scene.shape.SVGPath;
 
 public class CustomIcon {
     public String size;
+    public int sizeInt;
+    public String iconName;
     public Color color;
     public FontAwesomeIcons icon;
     public Controller controller;
 
+    // constructor for font awesome icons with a click event
     public CustomIcon(FontAwesomeIcons icon, String size, Color color, Controller controller) {
         this.size = size;
         this.color = color;
@@ -20,6 +24,22 @@ public class CustomIcon {
         this.controller = controller;
     }
 
+    // constructor for font awesome icons without a click event
+    public CustomIcon(FontAwesomeIcons icon, String size, Color color) {
+        this.size = size;
+        this.color = color;
+        this.icon = icon;
+    }
+
+    // constructor for icons from file without a click event
+    public CustomIcon(String iconName, int size, Color color) {
+        this.iconName = iconName;
+        this.sizeInt = size;
+        this.color = color;
+    }
+
+
+    // this method is used to build the font awesome icon with a click event
     public FontAwesomeIcon build(String type) {
         FontAwesomeIcon icon = new FontAwesomeIcon();
         icon.setSize(this.size);
@@ -31,9 +51,28 @@ public class CustomIcon {
             icon.setOnMouseClicked(e -> ((SearchIconController) this.controller).search());
         }
 
+        return icon;
+    }
+
+    // this method is used to build the font awesome icon without a click event
+    public FontAwesomeIcon build() {
+        FontAwesomeIcon icon = new FontAwesomeIcon();
+        icon.setSize(this.size);
+        icon.setFill(this.color);
+        icon.setIcon(this.icon);
 
         return icon;
     }
 
+    // this method is used to build the icon from file without a click event
+    public SVGPath build(String type, String path) {
+        SVGPath icon = new SVGPath();
+        icon.setContent(path);
+        icon.setFill(this.color);
+        icon.setScaleX(this.sizeInt);
+        icon.setScaleY(this.sizeInt);
+
+        return icon;
+    }
 
 }
