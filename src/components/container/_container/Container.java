45a83d5c58e551972cc0ba6b4e506a3cf72646fc
Dagim G. Astatkie <workflow.jd@gmail.com>
2023-04-21 16:23:01 +0300

    add: add new constructor for container class for the forms

--- src/components/container/_container/Container.java
+++ src/components/container/_container/Container.java
@@ -1,10 +1,15 @@
 package components.container._container;
 
 import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.Pane;
 
 public abstract class Container extends Pane{
   public Container(Scene scene, double widthPercentage, double heightPercentage) {
+    super();
+  }
+
+  public Container(AnchorPane root, double formWidth, double formHeight) {
     super();
   }
 
