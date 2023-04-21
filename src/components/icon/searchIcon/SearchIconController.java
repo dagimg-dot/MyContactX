a469360c36521a700c6ec3c1a8c2fefeb27d097a
Dagim G. Astatkie <workflow.jd@gmail.com>
2023-04-21 23:13:37 +0300

    refactor: apply the change that comes with controller interface

--- src/components/icon/searchIcon/SearchIconController.java
+++ src/components/icon/searchIcon/SearchIconController.java
@@ -2,9 +2,13 @@ package components.icon.searchIcon;
 
 import controller.Controller;
 
-public class SearchIconController extends Controller {
+public class SearchIconController implements Controller {
     public SearchIconController() {
         super();
+    }
+
+    public void onClick() {
+        search();
     }
 
     public void search() {
