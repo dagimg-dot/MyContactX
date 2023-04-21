69c3a009e3e0b06d495d88446fae1ad3059f37cf
Dagim G. Astatkie <workflow.jd@gmail.com>
2023-04-21 23:06:51 +0300

    create: create save button controller

---
+++ src/components/button/saveButton/SaveButtonController.java
@@ -0,0 +1,10 @@
+package components.button.saveButton;
+
+import controller.Controller;
+
+public class SaveButtonController implements Controller {
+    public void onClick() {
+        // Test
+        System.out.println("Save button clicked");
+    }
+}
