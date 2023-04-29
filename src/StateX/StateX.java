0981807e10c35bea508cadef9b67aabcc16c5521
Dagim G. Astatkie <workflow.jd@gmail.com>
2023-04-30 01:17:02 +0300

    add: add variables needed in the global state

---
+++ src/StateX/StateX.java
@@ -0,0 +1,12 @@
+package StateX;
+
+import javafx.collections.ObservableList;
+import javafx.scene.layout.VBox;
+import models.Contact;
+
+public class StateX {
+    public static int selectedContactIndex = -1;
+    public static VBox contactVBox;
+    public static ObservableList<Contact> contacts;
+
+}
