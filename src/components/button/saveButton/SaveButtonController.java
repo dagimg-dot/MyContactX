5700da788c043d7dc2eba2f192e2e5e2857041c5
Dagim G. Astatkie <workflow.jd@gmail.com>
2023-04-30 12:13:57 +0300

    refactor: handle formfields for new contacts

--- src/components/button/saveButton/SaveButtonController.java
+++ src/components/button/saveButton/SaveButtonController.java
@@ -1,10 +1,34 @@
 package components.button.saveButton;
 
+import java.util.Map;
+
+import components.container.contactList.ContactListController;
+import components.textfield.formfield.FormField;
 import controller.Controller;
 
 public class SaveButtonController implements Controller {
+    public Map<String, FormField> newContactFormFields;
+
+    public SaveButtonController(Map<String, FormField> newContactFormFields) {
+        this.newContactFormFields = newContactFormFields;
+    }
+
     public void onClick() {
         // Test
         System.out.println("Save button clicked");
+
+        // print the values of the form fields
+        newContactFormFields.forEach((key, value) -> {
+            System.out.println(key + ": " + value.getValue());
+        });
+        
+        // call the ContactListController to update the contact list
+        ContactListController contactListController = new ContactListController();
+        contactListController.addContact(newContactFormFields);
+
+        // clear the form fields
+        newContactFormFields.forEach((key, value) -> {
+            value.clear();
+        });
     }
 }
