47705a15db63b1a17458f9e0fe1eb5db6a21b742
Dagim G. Astatkie <workflow.jd@gmail.com>
2023-05-03 21:37:13 +0300

    refactor: nothing😂

--- src/components/textfield/_textfield/TextFieldListener.java
+++ src/components/textfield/_textfield/TextFieldListener.java
@@ -1,6 +1,7 @@
 package components.textfield._textfield;
 
 public class TextFieldListener {
+
     public TextFieldListener(CustomTextField textField) {
         textField.textProperty().addListener((observable, oldValue, newValue) -> {
             textField.setValue(newValue);
