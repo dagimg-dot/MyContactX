c338a08a96fa0a5ca74de2c8a4bf63e4f48ffefc
Dagim G. Astatkie <workflow.jd@gmail.com>
2023-05-04 01:06:55 +0300

    add: database class that handles connection with the database

--- src/services/Database.java
+++ src/services/Database.java
@@ -1,23 +1,40 @@
 package services;
 
-import java.util.ArrayList;
 
-import models.Contact;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 
 public class Database {
-    public Database() {
+
+    private static final String DB_URL = "jdbc:mysql://localhost:3306/mycontactx";
+    private static final String DB_USERNAME = "root";
+    private static final String DB_PASSWORD = "";
+
+    private static Connection connection;
+
+    public static Connection getConnection() {
+        if (connection == null) {
+            try {
+                connection = DriverManager.getConnection(DB_URL, DB_USERNAME, DB_PASSWORD);
+                System.out.println("Connected to database");
+            } catch (SQLException e) {
+                System.out.println("Failed to connect to database: " + e.getMessage());
+            }
+        }
+        return connection;
     }
 
-    public ArrayList<Contact> fetchContacts() {
-        ArrayList<Contact> contactList = new ArrayList<Contact>();
-        
-        // create 5 contact manually
-        contactList.add(new Contact("John Doe", "123456789", "Family"));
-        contactList.add(new Contact("Jane Doe", "123456789", "Family"));
-        contactList.add(new Contact("John Smith", "123456789", "Family"));
-        contactList.add(new Contact("Jane Smith", "123456789", "Family"));
-        contactList.add(new Contact("John Doe", "123456789", "Family"));
-
-        return contactList;
+    public static void closeConnection() {
+        if (connection != null) {
+            try {
+                connection.close();
+                System.out.println("Database connection closed");
+            } catch (SQLException e) {
+                System.out.println("Failed to close database connection: " + e.getMessage());
+            }
+        }
     }
+
 }
