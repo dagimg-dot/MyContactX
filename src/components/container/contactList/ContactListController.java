768088798462fe8de914cb1b4bd16cbd3a1f50a3
Dagim G. Astatkie <workflow.jd@gmail.com>
2023-05-04 01:06:04 +0300

    add: database connection and CRUD

--- src/components/container/contactList/ContactListController.java
+++ src/components/container/contactList/ContactListController.java
@@ -1,18 +1,22 @@
 package components.container.contactList;
 
+import java.sql.SQLException;
 import java.util.ArrayList;
 
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.scene.layout.HBox;
 import models.Contact;
+import services.ContactService;
 import StateX.StateX;
 
 public class ContactListController {
+    private ContactService contactService = ContactService.getInstance();
+
     public ContactListController() {
     }
 
-    public ObservableList<Contact> showContactList() {
+    public ObservableList<Contact> showContactList() throws SQLException {
         new Contact();
         ArrayList<Contact> contactList = Contact.getContactList();
         
@@ -21,18 +25,21 @@ public class ContactListController {
         return contacts;
     }
 
-    public void addContact() {
+    public void addContact() throws SQLException {
         System.out.println("Add Contact"); 
 
         // create a new contact
         Contact newContact = new Contact(
             StateX.newContactFormFields.get("Name").value,
+            StateX.newContactFormFields.get("Email").value,
             StateX.newContactFormFields.get("Phone").value,
-            StateX.newContactFormFields.get("Group").value          
+            StateX.newContactFormFields.get("City").value,  
+            StateX.newContactFormFields.get("Group").value
         );
         
         Contact.addContact(newContact);
         StateX.contacts.add(newContact);
+        contactService.saveContact(newContact);
     }
 
     public void fillContactForm() {
@@ -44,13 +51,15 @@ public class ContactListController {
         
         // load the values from the contact model into the form fields
         StateX.editContactFormFields.get("Name").setText(selectedContact.getName());
+        StateX.editContactFormFields.get("Email").setText(selectedContact.getEmail());
         StateX.editContactFormFields.get("Phone").setText(selectedContact.getPhone_no());
+        StateX.editContactFormFields.get("City").setText(selectedContact.getCity());
         StateX.editContactFormFields.get("Group").setText(selectedContact.getGroup());
         
         System.out.println("Form Filled");
     }
 
-    public void editContact() {
+    public void editContact() throws SQLException {
         System.out.println("Edit Contact");
 
         // get the selected contact
@@ -58,25 +67,30 @@ public class ContactListController {
         
         // update the contact model with the new values
         selectedContact.setName(StateX.editContactFormFields.get("Name").value);
+        selectedContact.setEmail(StateX.editContactFormFields.get("Email").value);
         selectedContact.setPhone_no(StateX.editContactFormFields.get("Phone").value);
+        selectedContact.setCity(StateX.editContactFormFields.get("City").value);
         selectedContact.setGroup(StateX.editContactFormFields.get("Group").value);
 
         // update the contact list
         Contact.getContactList().set(StateX.selectedContactIndex, selectedContact);
         StateX.contacts.set(StateX.selectedContactIndex, selectedContact);
+        contactService.updateContact(selectedContact);
         StateX.selectedContactIndex = -1;
 
     }
 
-    public void deleteContact() {
+    public void deleteContact() throws SQLException {
         System.out.println("Delete Contact");
         
         
         if (StateX.selectedContactIndex != -1) {
-            HBox selectedContact = (HBox) StateX.contactVBox.getChildren().get(StateX.selectedContactIndex);
-            StateX.contactVBox.getChildren().remove(selectedContact);
-            Contact.getContactList().remove(StateX.selectedContactIndex);
+            HBox selectedContactHBox = (HBox) StateX.contactVBox.getChildren().get(StateX.selectedContactIndex);
+            StateX.contactVBox.getChildren().remove(selectedContactHBox);
+            Contact selectedContact = Contact.getContactList().get(StateX.selectedContactIndex);
 			StateX.contacts.remove(StateX.selectedContactIndex);
+            Contact.getContactList().remove(StateX.selectedContactIndex);
+            contactService.deleteContact(selectedContact);
 			StateX.selectedContactIndex = -1;
         }
     }
--- src/models/Contact.java
+++ src/models/Contact.java
@@ -1,23 +1,43 @@
 package models;
 
+import java.sql.SQLException;
 import java.util.ArrayList;
 
-import services.Database;
+import services.ContactService;
 
 public class Contact {
-    public String name;
-    public String phone_no;
-    public String group;
+    private int id;
+    private String name;
+    private String email;
+    private String phone_no;
+    private String city;
+    private String group;
     public static ArrayList<Contact> contactList = new ArrayList<Contact>();   
+    private static ContactService contactService = ContactService.getInstance();
 
-    public Contact() {
-        contactList = new Database().fetchContacts();
+    public Contact() throws SQLException {
+        contactList = contactService.fetchContacts();
     }
 
-    public Contact(String name, String phone_no, String group) {
+    public Contact(int id,String name, String email, String phone_no, String city, String group) {
+        this.id = id;
         this.name = name;
+        this.email = email;
         this.phone_no = phone_no;
+        this.city = city;
         this.group = group;
+    }
+
+    public Contact(String name, String email, String phone_no, String city, String group) {
+        this.name = name;
+        this.email = email;
+        this.phone_no = phone_no;
+        this.city = city;
+        this.group = group;
+    }
+    
+    public int getId() {
+        return id;
     }
 
     public String getName() {
@@ -32,12 +52,28 @@ public class Contact {
         return group;
     }
 
+    public String getEmail() {
+        return email;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
     public void setName(String name) {
         this.name = name;
     }
 
     public void setPhone_no(String phone_no) {
         this.phone_no = phone_no;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
     }
 
     public void setGroup(String group) {
