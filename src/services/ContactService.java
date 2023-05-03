280a7a4270cbda1cf937ad8f5847655142ec3d00
Dagim G. Astatkie <workflow.jd@gmail.com>
2023-05-04 01:06:33 +0300

    add: new class that handles connection with the contact controller

---
+++ src/services/ContactService.java
@@ -0,0 +1,104 @@
+package services;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+import models.Contact;
+
+public class ContactService {
+    private Connection connection;
+    private static ContactService instance = null;
+
+    private ContactService() {
+    }
+
+    public static ContactService getInstance() {
+        if (instance == null) {
+            instance = new ContactService();
+        }
+        return instance;
+    }
+
+    public ArrayList<Contact> fetchContacts() throws SQLException {
+        ArrayList<Contact> contacts = new ArrayList<>();
+
+        connection = Database.getConnection();
+
+        String query = "SELECT * FROM contact";
+        PreparedStatement statement = connection.prepareStatement(query);
+
+        ResultSet resultSet = statement.executeQuery();
+
+        while (resultSet.next()) {
+            int id = resultSet.getInt("id");
+            String name = resultSet.getString("name");
+            String email = resultSet.getString("email");
+            String phone = resultSet.getString("phone");
+            String city = resultSet.getString("city");
+            String group = resultSet.getString("groupname");
+
+            Contact contact = new Contact(id,name, email, phone, city, group);
+            contacts.add(contact);
+        }
+
+        resultSet.close();
+        statement.close();
+
+        return contacts;
+    }
+
+    public void saveContact(Contact contact) throws SQLException {
+        connection = Database.getConnection();
+
+        String query = "INSERT INTO contact (name, email, phone, city, groupname) VALUES (?, ?, ?, ?, ?)";
+        PreparedStatement statement = connection.prepareStatement(query);
+
+        statement.setString(1, contact.getName());
+        statement.setString(2, contact.getEmail());
+        statement.setString(3, contact.getPhone_no());
+        statement.setString(4, contact.getCity());
+        statement.setString(5, contact.getGroup());
+
+        statement.executeUpdate();
+
+        statement.close();
+    }
+
+    public void deleteContact(Contact contact) throws SQLException {
+        connection = Database.getConnection();
+
+        String query = "DELETE FROM contact WHERE id = ?";
+        PreparedStatement statement = connection.prepareStatement(query);
+
+        statement.setInt(1, contact.getId());
+
+        statement.executeUpdate();
+
+        statement.close();
+    }
+
+    public void updateContact(Contact contact) throws SQLException {
+        connection = Database.getConnection();
+
+        String query = "UPDATE contact SET name = ?, email = ?, phone = ?, city = ?, groupname = ? WHERE id = ?";
+        PreparedStatement statement = connection.prepareStatement(query);
+
+        statement.setString(1, contact.getName());
+        statement.setString(2, contact.getEmail());
+        statement.setString(3, contact.getPhone_no());
+        statement.setString(4, contact.getCity());
+        statement.setString(5, contact.getGroup());
+
+        statement.setInt(6, contact.getId());
+
+
+        statement.executeUpdate();
+
+        statement.close();
+    }
+
+
+}
