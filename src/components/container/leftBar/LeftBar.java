3b5d471bcad759b1c0958b6e4c7aa3e5a791fb93
Dagim G. Astatkie <workflow.jd@gmail.com>
2023-08-17 21:49:07 +0300

    some progress on the detail bar

--- src/components/container/leftBar/LeftBar.java
+++ src/components/container/leftBar/LeftBar.java
@@ -7,7 +7,10 @@ import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
 import javafx.scene.text.Text;
 
 public class LeftBar extends Container{
@@ -41,8 +44,9 @@ public class LeftBar extends Container{
         // create the title text
         Text details = TextGenerator.generateText("Details","Poppins-SemiBold",20,"#554D4D");
 
-        // set the alignment of the title
+        // set the alignment and style of the title
         title.setAlignment(Pos.CENTER);
+        title.setStyle("-fx-background-color: #E3E0E0; -fx-background-radius: 15;");
         
         // add the title text to the title HBox
         title.getChildren().add(details);
@@ -50,11 +54,49 @@ public class LeftBar extends Container{
         // set margin for the title
         VBox.setMargin(title, new Insets(10,10,10,10));
 
-        // set the style of the title
-        title.setStyle("-fx-background-color: #E3E0E0; -fx-background-radius: 15;");
+        // create details wrapper for the VBox
+        StackPane detailsWrapper = new StackPane();
+
+
+        // set the width and height of the details container
+        detailsWrapper.prefWidthProperty().bind(leftBarContainer.prefWidthProperty());
+        detailsWrapper.setPrefHeight(this.getPrefHeight() / 2);
+
+        // add listener to the left bar container to update the details container
+        leftBarContainer.heightProperty().addListener((obs, oldVal, newVal) -> {
+            detailsWrapper.setPrefHeight(newVal.doubleValue() / 2);
+        });
+
+        // create the details container for the selected contact
+        VBox detailsContainer = new VBox();
+        
+        // set the width and height of the details container
+        detailsContainer.prefWidthProperty().bind(detailsWrapper.prefWidthProperty());
+        detailsContainer.prefHeightProperty().bind(detailsWrapper.prefHeightProperty());
+
+        Circle contactPhoto = new Circle(50, 50, 50);
+        contactPhoto.setFill(Color.web("#A39696"));
+
+        HBox contactPhotoContainer = new HBox();
+        contactPhotoContainer.setAlignment(Pos.CENTER);
+        contactPhotoContainer.getChildren().add(contactPhoto);
+
+        VBox.setMargin(contactPhotoContainer, new Insets(10,10,10,10));
+
+        detailsContainer.getChildren().add(contactPhotoContainer);
+
+        detailsWrapper.getChildren().add(detailsContainer); 
+
+
+        // set the style of the details container
+        detailsWrapper.setStyle("-fx-background-color: #E3E0E0; -fx-background-radius: 15;");
+
+        // set margin for the details container
+        VBox.setMargin(detailsWrapper, new Insets(0,10,10,10));
+
 
         // add the title to the left bar container
-        leftBarContainer.getChildren().addAll(title);
+        leftBarContainer.getChildren().addAll(title, detailsWrapper);
         this.getChildren().addAll(leftBarContainer);
 
     };
